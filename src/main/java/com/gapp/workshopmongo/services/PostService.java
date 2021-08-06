64330e04c312af39c84cac0ce4c9ac0f2e7dd237
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-06 16:48:53 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gapp/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gapp.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gapp.workshopmongo.domain.Post;
+import com.gapp.workshopmongo.repository.PostRepository;
+import com.gapp.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
