0e112f4e09ab5c2e8b54737c97fdfaa74a2f994d
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-06 16:27:04 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/gapp/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gapp/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gapp.workshopmongo.domain.Post;
 import com.gapp.workshopmongo.domain.User;
+import com.gapp.workshopmongo.dto.AuthorDTO;
 import com.gapp.workshopmongo.repository.PostRepository;
 import com.gapp.workshopmongo.repository.UserRepository;
 
@@ -29,11 +30,12 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Parviu viagem", "Vou viajar para Sâo Paulo. Abraços!", maria);
-		Post post2 = new Post(null, Instant.parse("2018-03-23T11:41:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Parviu viagem", "Vou viajar para Sâo Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, Instant.parse("2018-03-23T11:41:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
