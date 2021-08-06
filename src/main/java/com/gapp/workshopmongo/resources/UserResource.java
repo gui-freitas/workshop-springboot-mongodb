611db692d45c9529927dd4ee095cbe2a9900afbe
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-06 09:25:01 -0300

    Atualização de usuário com PUT

--- src/main/java/com/gapp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gapp/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +52,13 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@RequestBody UserDTO userDto, @PathVariable String id) {
+		User user = userService.fromDTO(userDto);
+		user.setId(id);
+		user = userService.update(user);
+		return ResponseEntity.ok().body(user);
 	}
 }
